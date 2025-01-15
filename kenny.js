6ab3df7d895826ac76bd21a0bda06b1970b69340
spectrum24c <Awokojorichmond@gmail.com>
2025-01-15 15:24:01 +0100

    first commit

--- kenny.js
+++ kenny.js
@@ -19,5 +19,18 @@ window.addEventListener('DOMContentLoaded', ()=>{
         span.classList.add('active');
       }, (idx + 1)* 800)
     });
+    setTimeout(()=>{
+
+      logoSpan.forEach((span, idx)=>{
+        setTimeout(()=>{
+          span.classList.remove('active');
+          span.classList.add('fade');
+        }, (idx + 1)* 200)
+      })
+    },2000);
+
+    setTimeout(()=>{
+      intro.style.top = '-100vh';
+    },3300)
   })
 })
